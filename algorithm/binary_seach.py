aecf93dd6943b65c35633bfd02620464cc2c44ed
Xu <eric.zhuxu@gmail.com>
2023-03-03 01:49:45 +0800

    [DEFAULT] regular updates

---
+++ algorithm/binary_seach.py
@@ -0,0 +1,17 @@
+def binary_search(sorted_seq, val):
+    low = 0
+    high = len(sorted_seq) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if sorted_seq[mid] == val:
+            return mid
+        elif sorted_seq[mid] > val:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return low
+
+#---------------------------------------------------------------------------------------------------
+l1 = [1, 2, 3, 6, 9, 12, 22, 33, 40, 50, 52]
+index = binary_search(l1, 5)
+print(index)
---
+++ data-structure/linked_list.py
@@ -0,0 +1,6 @@
+class ListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+    
